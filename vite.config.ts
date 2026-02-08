569f5156838e4e68942590fd05a500dff7df8046
Bigmints <66945811+bigmints@users.noreply.github.com>
2026-02-08 18:18:16 +0400

    Add cache-busting to app icon URLs

--- vite.config.ts
+++ vite.config.ts
@@ -20,25 +20,25 @@ export default defineConfig({
         theme_color: '#0D1252',
         icons: [
           {
-            src: 'assets/appicon.png',
+            src: 'assets/appicon.png?v=2',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'assets/appicon.png',
+            src: 'assets/appicon.png?v=2',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'assets/appicon.png',
+            src: 'assets/appicon.png?v=2',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
           },
           {
-            src: 'assets/appicon.png',
+            src: 'assets/appicon.png?v=2',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
